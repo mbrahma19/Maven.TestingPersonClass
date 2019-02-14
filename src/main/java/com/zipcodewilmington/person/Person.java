898db908435eb1842646155c334e9cdee8006115
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-8.local>
2019-02-13 20:30:47 -0500

    Completed adding new fields and test methods

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,91 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private Person mom;
+    private Person dad;
+    private boolean isFemale;
+    private double height;
+    private double weight;
+
 
     public Person() {
+        this("",Integer.MAX_VALUE);
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(double height) {
+        this.height = height;
+    }
+
+    public Person(boolean b) {
+        this.isFemale = b;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
+    }
+
+    public void setDad(Person dad) {
+        this.dad = dad;
+    }
+
+    public Person getDad() {
+        return this.dad;
+    }
+
+    public Person getMom() {
+        return this.mom;
+    }
+
+    public void setMom(Person mom) {
+        this.mom = mom;
+    }
+
+    public Double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Double getWeight() {
+        return this.weight;
+    }
+
+    public void setIsFemale(boolean b) {
+        this.isFemale = b;
+    }
+
+    public boolean getIsFemale() {
+        return this.isFemale;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -69,6 +69,23 @@ public class TestPerson {
     }
 
     @Test
+    public void testConstructorWithNameAge() {
+        // Given
+        Integer expectedAge = 5;
+        String expectedName = "Leon";
+
+        // When
+        Person person = new Person(expectedName, expectedAge);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
     public void testSetName() {
         // Given
         Person person = new Person();
@@ -86,7 +103,34 @@ public class TestPerson {
     public void testSetAge() {
         // Given
         Person person = new Person();
-        Integer expected = 5;
+        Integer expected = 26;
+
+        // When
+        person.setAge(26);
+
+        // Then
+        Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetName() {
+        // Given
+        Person person = new Person("Marci");
+        String expected = "Marci";
+
+        // When
+        String actual = person.getName();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAge() {
+        // Given
+        Person person = new Person(26);
+        Integer expected = 26;
 
         // When
         person.setAge(expected);
@@ -95,4 +139,141 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSetDad() {
+        // Given
+        Person person = new Person();
+        Person dad = new Person();
+        Person expected = dad;
+
+        // When
+        person.setDad(dad);
+
+        // Then
+        Person actual = person.getDad();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetDad() {
+        // Given
+        Person person = new Person();
+        Person expected = new Person("Dad");
+
+        // When
+        person.setDad(expected);
+        Person actual = person.getDad();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetMom() {
+        // Given
+        Person person = new Person();
+        Person mom = new Person();
+        Person expected = mom;
+
+        // When
+        person.setMom(mom);
+
+        // Then
+        Person actual = person.getMom();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetMom() {
+        // Given
+        Person person = new Person();
+        Person expected = new Person("Mom");
+
+        // When
+        person.setMom(expected);
+        Person actual = person.getMom();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        Double expected = 5.1;
+
+        // When
+        person.setHeight(5.1);
+
+        // Then
+        Double actual = person.getHeight();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetHeight() {
+        // Given
+        Person person = new Person(6.4);
+        Double expected = 6.4;
+
+        // When
+        Double actual = person.getHeight();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetWeight() {
+        // Given
+        Person person = new Person();
+        Double expected = 150.0;
+
+        // When
+        person.setWeight(150.0);
+
+        // Then
+        Double actual = person.getWeight();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetWeight() {
+        // Given
+        Person person = new Person();
+        Double expected = 135.0;
+        person.setWeight(135.0);
+
+        // When
+        Double actual = person.getWeight();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetIsFemale() {
+        // Given
+        Person person = new Person();
+        boolean expected = true;
+
+        // When
+        person.setIsFemale(true);
+
+        // Then
+        boolean actual = person.getIsFemale();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetIsFemale() {
+        // Given
+        Person person = new Person(false);
+        boolean expected = false;
+
+        // When
+        boolean actual = person.getIsFemale();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
